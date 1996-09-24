f24a30cae705c8e7d1022ffb3b64a0d8691d8f79
fdrake <fdrake>
1996-09-24 13:49:06 +0000

    (text_html.py, text_plain.py): 	Wrapper modules that allow for extensibility of the text/html and 	text/plain parsers; users can customize to their hearts content, 	and actually subclass from the GrailHTMLParser if desired!

---
+++ src/filetypes/text_plain.py
@@ -0,0 +1,11 @@
+"""Grail parser for text/plain.
+"""
+__version__ = '$Revision: 2.1 $'
+#  $Source: /cvsroot/grail/grail/src/filetypes/text_plain.py,v $
+
+import Reader
+
+parse_text_plain = Reader.TextParser
+
+#
+#  end of file
