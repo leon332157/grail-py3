06ed6a09e04fae4d97eb5143875de67d4ee6ef96
Martin Panter <vadmium  gmailcom>
2013-04-08 21:31:25 +1000

    Rewrite JPEG parser to avoid mktemp() and CLI encoding issues

--- grail/filetypes/image_jpeg.py
+++ grail/filetypes/image_jpeg.py
@@ -1,58 +1,62 @@
 import tempfile
 import os
-from grailutil import getenv, which
 from Tkinter import *
 from formatter import AS_IS
-from os import getenv
+import subprocess
+from ..grailutil import close_subprocess
 
 _FILTERCMD = 'djpeg'
 _FILTERARG = '-gif'
-_FILTERPATH = which(_FILTERCMD, getenv('PATH').split(':'))
 
-if hasattr(os, 'popen') and _FILTERPATH:
-    _FILTER = _FILTERPATH + ' ' + _FILTERARG
- 
-    class parse_image_jpeg:
-    
-        """Parser for image/jpeg files.
-    
-        Collect all the data in a temp file and then create an in-line
-        image from it.
-    
-        """
-    
-        def __init__(self, viewer, reload=False):
-            self.broken = None
-            self.tf = self.tfname = None
-            self.viewer = viewer
-            self.viewer.new_font((AS_IS, AS_IS, AS_IS, True))
-            self.tfname = tempfile.mktemp()
-            self.tf = os.popen(_FILTER + '>' + self.tfname, 'wb')
-            self.label = Label(self.viewer.text, text=self.tfname,
+class parse_image_jpeg:
+
+    """Parser for image/jpeg files.
+
+    Collect all the data in a temp file and then create an in-line
+    image from it.
+
+    """
+
+    def __init__(self, viewer, reload=False):
+        self.broken = None
+        self.tf = self.proc = None
+        self.viewer = viewer
+        self.viewer.new_font((AS_IS, AS_IS, AS_IS, True))
+        self.tf = tempfile.NamedTemporaryFile("wb", delete=False)
+        try:
+            self.proc = subprocess.Popen((_FILTERCMD, _FILTERARG),
+                stdin=subprocess.PIPE, stdout=self.tf, bufsize=-1)
+            self.tf.close()
+            self.label = Label(self.viewer.text, text=self.tf.name,
                                highlightthickness=0, borderwidth=0)
             self.viewer.add_subwindow(self.label)
-    
-        def feed(self, data):
-            try:
-                self.tf.write(data)
-            except IOError:
-                self.tf.close()
-                self.tf = None
-                self.broken = True
-                raise
-    
-        def close(self):
-            if self.tf:
-                self.tf.close()
-                self.tf = None
-                self.label.image = PhotoImage(file=self.tfname)
-                self.label.config(image=self.label.image)
-            try:
-                os.unlink(self.tf.name)
-            except os.error:
-                pass
-            if self.broken:
-                # TBD: horrid kludge... don't hate me! ;-)
-                self.label.image = PhotoImage(file='icons/sadsmiley.gif')
-                self.label.config(image=self.label.image)
-                self.viewer.text.insert(END, '\nBroken Image!')
+        except:
+            self.tf.close()
+            if self.proc:
+                close_subprocess(self.proc)
+            raise
+
+    def feed(self, data):
+        try:
+            self.proc.stdin.write(data)
+        except IOError:
+            close_subprocess(self.proc)
+            self.proc = None
+            self.broken = True
+            raise
+
+    def close(self):
+        if self.proc:
+            close_subprocess(self.proc)
+            self.proc = None
+            self.label.image = PhotoImage(file=self.tf.name)
+            self.label.config(image=self.label.image)
+        try:
+            os.unlink(self.tf.name)
+        except os.error:
+            pass
+        if self.broken:
+            # TBD: horrid kludge... don't hate me! ;-)
+            self.label.image = PhotoImage(file='icons/sadsmiley.gif')
+            self.label.config(image=self.label.image)
+            self.viewer.text.insert(END, '\nBroken Image!')
